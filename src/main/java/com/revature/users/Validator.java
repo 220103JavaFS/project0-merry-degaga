2562a5d28d6e1261756c02fc9c3dcecf9be483de
merry-degaga <merry.degaga@revature.net>
2022-01-11 17:59:30 -0500

    completed Validator class for now, will need to add in throw new Exception when validation fails

--- src/main/java/com/revature/users/Validator.java
+++ src/main/java/com/revature/users/Validator.java
@@ -1,5 +1,7 @@
 package com.revature.users;
 
+import java.util.Locale;
+
 public final class Validator {
 
     private static String pattern;
@@ -24,7 +26,7 @@ public final class Validator {
         if(isEmpty(email)) {
             //throw new Exception();
         }
-        pattern = "[A-Za-z]{1,32}([A-Za-z]|[0-9]){0,32}@[A-Za-z]{1,121}([A-Za-z]|[0-9]){0,121}.[a-z]{3,3}";
+        pattern = "[A-Za-z]{1,32}([A-Za-z]|[0-9]){0,32}@[A-Za-z]{1,121}([A-Za-z]|[0-9]){0,121}.[a-z]{3}";
         if(!email.matches(pattern)){
             //throw new exception
         }
@@ -32,7 +34,7 @@ public final class Validator {
     }
 
     static boolean isValidPhoneNumber(String phoneNumber){
-        pattern = "[1-9]{3,3}.\\d{3,3}.\\d{4,4}";
+        pattern = "[1-9]{3}.\\d{3}.\\d{4}";
         if(!phoneNumber.matches(pattern)){
             //throw exception
         }
@@ -40,10 +42,32 @@ public final class Validator {
     }
 
     static boolean isValidDOB(String dob){
-        //format month/date
-        //month abbr month/1-...
-        pattern = "";
-        return false;
+        pattern = "([1-9]|([1][0-2]))";
+
+        int slash = dob.indexOf('/');
+        String month = dob.substring(0,slash);
+
+        if(!month.matches(pattern)) {
+            //throw error invalid month
+        }
+
+        //month is valid so check date
+
+        switch (month) {
+            case "2": break;
+            case "7":
+            case "6":
+            case "9":
+            case "11":pattern = "[/]((([1-9])|([1-2][0-9]))|30)";
+                      break;
+            default: pattern = "[/]((([1-9])|([1-2][0-9]))|30|31)";
+                break;
+        }
+
+        if(!dob.matches(pattern)) {
+            //throw error bad date
+        }
+        return true;
     }
 
     private static boolean isEmpty(String input){
