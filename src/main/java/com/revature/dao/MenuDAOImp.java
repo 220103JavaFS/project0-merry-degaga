ee2ab517ba3e2347d2420bba77aac8b158a5405a
merry-degaga <merry.degaga@revature.net>
2022-01-15 12:46:39 -0500

    updated menuDaoimp

--- src/main/java/com/revature/dao/MenuDAOImp.java
+++ src/main/java/com/revature/dao/MenuDAOImp.java
@@ -1,20 +1,36 @@
 package com.revature.dao;
 
 import com.revature.users.cart.food.Food;
+import com.revature.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class MenuDAOImp implements MenuDAO{
     @Override
     public void addMenuItem(Food food) {
+        try(Connection conn = ConnectionUtil.getConnection()){
 
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeMenuItem(String name) {
+        try(Connection conn = ConnectionUtil.getConnection()){
 
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void editMenuItem(String name) {
+        try(Connection conn = ConnectionUtil.getConnection()){
 
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
