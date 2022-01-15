394bb04a08a4683a87d8598024aefcf8e58e791a
merry-degaga <merry.degaga@revature.net>
2022-01-15 12:46:12 -0500

    updated logonDAOImp

--- src/main/java/com/revature/dao/LogonDAOImp.java
+++ src/main/java/com/revature/dao/LogonDAOImp.java
@@ -1,13 +1,29 @@
 package com.revature.dao;
 
+import com.revature.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class LogonDAOImp implements LogonDAO {
 
     public boolean logon(String username, String password){
+        try(Connection conn = ConnectionUtil.getConnection()){
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return false;
     }
 
     @Override
     public String getUserRole(String username) {
+        try(Connection conn = ConnectionUtil.getConnection()){
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
         return "Manager";
     }
 
