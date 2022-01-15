05b2f9b37703f11c917145ad6baae9a11e6a065c
merry-degaga <merry.degaga@revature.net>
2022-01-15 12:47:04 -0500

    updated orderdaoimp

--- src/main/java/com/revature/dao/OrderDAOImp.java
+++ src/main/java/com/revature/dao/OrderDAOImp.java
@@ -1,14 +1,27 @@
 package com.revature.dao;
 
+import com.revature.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class OrderDAOImp implements OrderDAO{
 
     @Override
     public void getAllOrders() {
+        try(Connection conn = ConnectionUtil.getConnection()){
 
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void completeOrder() {
+        try(Connection conn = ConnectionUtil.getConnection()){
 
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
