3182ceb2cedb41d48dbd6b828fd98af26cbfc0ee
merry-degaga <merry.degaga@revature.net>
2022-01-14 22:12:32 -0500

    updated OrderController

--- src/main/java/com/revature/controllers/OrderController.java
+++ src/main/java/com/revature/controllers/OrderController.java
@@ -3,22 +3,24 @@ import com.revature.service.OrderService;
 import io.javalin.Javalin;
 import io.javalin.http.Handler;
 
-
 public class OrderController extends Controller {
     private OrderService service = new OrderService();
     public OrderController(){}
 
     private Handler getAllOrders = (ctx) -> {
+        //service.getAllOrders();
+        ctx.status(200);
 
     };
     private Handler completeOrder = (ctx) -> {
-
+        //service.completeOrder(); //automatically completes first order in table.
+        ctx.status(200);
     };
 
     @Override
     public void addRoutes(Javalin app) {
-        app.get("/orders",getAllOrders, Role.MANAGER);
-        app.put("/complete", completeOrder, Role.MANAGER);
+        app.get("/orders",getAllOrders, Role.MANAGER, Role.EMPLOYEE);
+        app.delete("/complete", completeOrder, Role.MANAGER, Role.EMPLOYEE);
     }
 
 
