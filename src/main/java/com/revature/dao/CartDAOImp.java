0a3d6a11d63d020e47aecd4da4f25faf6c8bd8bc
merry-degaga <merry.degaga@revature.net>
2022-01-15 12:45:06 -0500

    updated cartDaoimp

--- src/main/java/com/revature/dao/CartDAOImp.java
+++ src/main/java/com/revature/dao/CartDAOImp.java
@@ -1,6 +1,10 @@
 package com.revature.dao;
 
 import com.revature.users.cart.food.Food;
+import com.revature.util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class CartDAOImp implements CartDAO{
 
@@ -9,20 +13,37 @@ public class CartDAOImp implements CartDAO{
     @Override
     public void addItemToCart(Food food) {
 
+        try(Connection conn = ConnectionUtil.getConnection()){
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeItemFromCart(String name) {
+        try(Connection conn = ConnectionUtil.getConnection()){
 
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void getTotalOrder() {
+        try(Connection conn = ConnectionUtil.getConnection()){
 
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void editItemInCart(String name) {
+        try(Connection conn = ConnectionUtil.getConnection()){
 
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
