f20ee49100a2d0c6058a296f0ac7626a9bfcc8ae
merry-degaga <merry.degaga@revature.net>
2022-01-14 22:17:52 -0500

    edited CartController

--- src/main/java/com/revature/controllers/CartController.java
+++ src/main/java/com/revature/controllers/CartController.java
@@ -1,6 +1,7 @@
 package com.revature.controllers;
 
 import com.revature.service.CartService;
+import com.revature.users.cart.food.Food;
 import io.javalin.Javalin;
 import io.javalin.http.Handler;
 
@@ -9,13 +10,19 @@ public class CartController extends Controller {
     public CartController(){}
 
     private Handler addItemToCart = (ctx) -> {
-
+        Food food = ctx.bodyAsClass(Food.class);
+        //service.addItemToCart(food);
     };
     private Handler removeItemFromCart = (ctx) -> {
-
+        String name = ctx.queryParam("name");
+        //service.removeItemFromCart(name);
     };
     private Handler getTotalOrder = (ctx) -> {
-
+        //service.getTotalOrder();
+    };
+    private Handler editItemInCart = (ctx) -> {
+        String name = ctx.queryParam("name");
+        //service.editItemInCart(name)
     };
 
     @Override
@@ -23,6 +30,7 @@ public class CartController extends Controller {
         app.get("/cart", getTotalOrder, Role.CUSTOMER, Role.EMPLOYEE, Role.MANAGER);
 
         app.put("/menu/add/cart", addItemToCart, Role.CUSTOMER, Role.EMPLOYEE, Role.MANAGER);
-        app.put("/menus/remove", removeItemFromCart, Role.CUSTOMER, Role.EMPLOYEE, Role.MANAGER);
+        app.delete("/menu/remove/cart", removeItemFromCart, Role.CUSTOMER, Role.EMPLOYEE, Role.MANAGER);
+        app.patch("/menus/edit/cart", editItemInCart, Role.CUSTOMER, Role.EMPLOYEE, Role.MANAGER);
     }
 }
